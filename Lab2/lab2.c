5f11642b1239fdb353675a8d9a494c6d4164a43a
Tvisha Gangwani <gangwani@usc.edu>
2019-02-24 15:48:54 -0500

    typing8

--- Lab2/lab2.c
+++ Lab2/lab2.c
@@ -130,7 +130,7 @@ int main()
       if (packet.keycode[0]) { /* Trying to princt a char */
       	if (packet.keycode[0] > 0x1d)
       	{
-      		char c  = packet.keycode[0] +13;
+      		char c  = packet.keycode[0] +19;
       		
 			fbputchar(c,row1, col1);
 			col1 = col1 +1 ;
