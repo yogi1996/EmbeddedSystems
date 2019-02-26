8af17afa417680cb3a72bfe8c7f5d3b93c4be41f
Tvisha Gangwani <gangwani@usc.edu>
2019-02-26 15:34:41 -0500

    cursor mv r

--- Lab2/lab2.c
+++ Lab2/lab2.c
@@ -359,7 +359,7 @@ else {
       fbputchar('_',row1,col1);
       if (packet.keycode[0] == 0x4f){
       	fbputchar(' ',row1, col1);
-      	m=m-1;
+      	//m=m-1;
       	//stringSend[m] = '\0';
       	fbputs(stringSend,21,1);
       	m = m+1;
