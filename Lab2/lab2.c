f0b9a6ac141a74e2e34dfeadec5d547798fff4c6
Tvisha Gangwani <gangwani@usc.edu>
2019-02-27 13:43:07 -0500

    that did not work trying new, agaimn

--- Lab2/lab2.c
+++ Lab2/lab2.c
@@ -484,24 +484,8 @@ void *network_thread_f(void *ignored)
   /* Receive data */
   
   while ( (n = read(sockfd, &recvBuf, BUFFER_SIZE - 1)) > 0 ) {
-  	recvBuf[n] = '\0';
+    recvBuf[n] = '\0';
     printf("HERE IS THE BUFFER %s", recvBuf);
-  	if (netRow == 20){
-    //Clear the table 
-    for (netCol = 0 ; netCol < 64 ; netCol++) {
-    	for (netRow = 0;netRow<21;netRow++){
-    	//fbputchar('*', 0, col);
-    		fbputchar(' ', netRow, netCol);
-    		}
-  		}
-  		
-    netRow = 1;
-    netCol = 0;
-    
-    //fbputs(recvBuf, netRow, 0);
-    netRow = 2;
-    }
-    
     char temp[65];
     if (n > 60){
     	
@@ -532,7 +516,21 @@ void *network_thread_f(void *ignored)
     netCol = 0;
     }
     
+    if (netRow == 21){
+    //Clear the table 
+    for (netCol = 0 ; netCol < 64 ; netCol++) {
+    	for (netRow = 0;netRow<21;netRow++){
+    	//fbputchar('*', 0, col);
+    		fbputchar(' ', netRow, netCol);
+    		}
+  		}
+  		
+    netRow = 1;
+    netCol = 0;
     
+    fbputs(recvBuf, netRow, 0);
+    netRow = 2;
+    }
     
     
   }
